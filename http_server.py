8bf7b3d9e5ad76f82a6b5052ee25cdcdf462dfc9
Michael Yang <yangxing20107@gmail.com>
2018-12-18 11:01:29 +0800

    Update http_server.py

--- http_server.py
+++ http_server.py
@@ -1,3 +1,32 @@
 
 
 
+from twisted.internet import protocol, reactor, endpoints
+
+class HTTP(protocol.Protocol):
+    def dataReceived(self, data):  
+        print("=> req data:\n%s\n" % data)
+        content = "...hello...http...fuck..."
+        response = b"""HTTP/1.0 200 OK
+Date: fuck
+Server: fuck
+Last-Modified: fuck
+Content-Length: %d
+Content-Type: text/plain
+Connection: Closed
+
+%s """ % (len(content), content)
+        print("=> send http response.")        
+        self.transport.write(response)
+        self.transport.loseConnection()  
+
+class HTTPFactory(protocol.Factory):
+    def buildProtocol(self, addr):
+        return HTTP()
+
+endpoints.serverFromString(reactor, "tcp:18000").listen(HTTPFactory())
+print("HTTP server started at :18000")
+reactor.run()
+
+
+
